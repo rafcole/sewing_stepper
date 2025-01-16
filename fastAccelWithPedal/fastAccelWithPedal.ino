6593405903f2db0f1516d1f30a850e65b9d854b4
rafcole <39354464+rafcole@users.noreply.github.com>
2025-01-15 19:31:22 -0800

    homing status flag refactored

--- fastAccelWithPedal/fastAccelWithPedal.ino
+++ fastAccelWithPedal/fastAccelWithPedal.ino
@@ -38,7 +38,10 @@ struct MachinePositions keyPositions = {0, 50, 85, 180, 240, 275};
 
 // pedal ranges from 3.5-5v 
 unsigned int pedalPinMax = 290;
-unsigned int pedalPinMin = 14;
+unsigned int pedalPinMin = 20;
+
+unsigned int reservedSingleStitchMin = 20;
+unsigned int reservedSingleStitchMax = 40;
 
 unsigned int minSpeedInHz = 5000; // 1700 good
 unsigned int maxSpeedInHz = 30000; // bog at 45k
@@ -62,6 +65,8 @@ long oldPosition  = -999;
 int encoderPulsesPerRotation = 2400;
 float pulsesPerDegree = float (encoderPulsesPerRotation) / 360;
 
+float stepsPerDegree = 5712.00 / 360;
+
 Encoder myEnc(encoderPinA, encoderPinB);
 
 // button press logic tester
@@ -81,45 +86,14 @@ int pedalReadMax = 0;
 
 // sampling of pedal signals to quash stray signals on the pedal
 int pedalSamples[5] = {0};
+bool homingSamples[10] = {false};
 
 long stepsPerRotation = 0;
 volatile bool firstRotationComplete = false;
 volatile bool makingFirstRotation = false;
 long firstHomeTimeStamp = 0;
 long secondHomeTimeStamp = 0;
-// add single sensor
-// Track current position in steps 
-// Need global variable 
-//   "getCurrentPosition() is counting monotonously up or down respectively."
-//   setCurrentPosition(num)
 
-// stepsPerRotation, intialize to 0
-// firstPass = initialize to false, set to true when first detection hits
-
-// interupt on ND sensor
-  // calibration
-    // set steps per rotation if not already set
-      // if firstPass is false
-        // turn to true, set current position to 0
-      // if stepPerRotation is not set
-        // stepsPerRotation = current position
-        // halfStep
-        // cushion
-
-  // if currentSpeed > minimum
-    // do nothing
-  // still sewing
-
-// TODO - ======================================== Do this first
-  // interupt on pedal goes low
-    // set this first, should work immediately?
-    // refactor main loop to run forward based on pin read
-
-  // interupts on pedal control runForward and forceStop?
-
-
-// calibrationPass
-  
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
@@ -138,91 +112,102 @@ void setup() {
 
   // ISR for external sensors
   pinMode(needleDownSensorPin, INPUT_PULLUP);
-  // attachInterrupt(digitalPinToInterrupt(needleDownSensorPin), needleDownInterrupt, RISING);
 
-  // default value gets erased
-  foundHome = false;
   lastZero = !digitalRead(needleDownSensorPin);
 }
 
+long pulsesTravelled = 0;
+
 void loop() {
-  int pedalReading = analogRead(pedalPin);
+  int pedalReading = updatePedalReading(analogRead(pedalPin));
 
-
-
-  // pedalReadCount++;
-  // pedalReadTotal += pedalReading;
-  // pedalReadAvg = pedalReadTotal / pedalReadCount;
-  // if (pedalReading > pedalReadMax) {
-  //   pedalReadMax = pedalReading;
-  // }
-  // if (millis() % 10 == 0) {
-  //   Serial.print("pedal_reading:");
-  //   Serial.print(pedalReading);
-  //   Serial.print(",");
-  //   Serial.print("Max_reading:");
-  //   Serial.print(pedalReadMax);
-  //   Serial.print(",");
-  //   Serial.print("Avg_reading:");
-  //   Serial.println(pedalReadAvg);
-  // }
-
-
-  // Serial.println(stepper->getCurrentPosition());
-  // delay(500);
   bool atZero = !digitalRead(needleDownSensorPin);
   bool zeroRising = atZero && !lastZero;
   lastZero = atZero;
 
+  pulsesTravelled += myEnc.read() - pulsesTravelled;
+
   if (zeroRising) {
-    // Serial.print(stepper->getCurrentPosition());
-    // Serial.println("Found Zero");
+    myEnc.write(0);
+    // Serial.print("encoder pos:");
+    // Serial.println(myEnc.read());
     stepper->setCurrentPosition(0);
   }
 
-  bool pedalUp = detectPedalUp(pedalReading);
+  bool pedalUp = pedalReading < pedalPinMin - 5;
 
-  unsigned int pedalSpeedInHz = map(pedalReading, pedalPinMin, pedalPinMax, minSpeedInHz, maxSpeedInHz);
-  // Serial.println(pedalSpeedInHz);
+  unsigned int pedalSpeedInHz;
 
-
-
-  
-  // bool pedalDown = pedalSpeedInHz >= minSpeedInHz;
-  if (!pedalUp) {
-    // Serial.print("PedalUp: ");
-    // Serial.println(pedalUp);
-    // Serial.println(pedalReading);
+  if (pedalUp) {
+    pedalSpeedInHz = 0;
+  } else {
+     pedalSpeedInHz = map(pedalReading, pedalPinMin, pedalPinMax, minSpeedInHz, maxSpeedInHz);
   }
 
-  // Serial.print("PedalUp: ");
-  // Serial.println(pedalUp);
-  // Serial.println(pedalReading);
-  // delay(300);
+  machineIsHoming = debounceHoming(pedalUp) && degreesTravelled() > 40 && !(stepper->isStopping());
 
-  machineIsHoming = pedalUp && stepper->isRunning();
-  bool machineIsHomingRising = machineIsHoming && !lastPedalUp;
+  Serial.print("\n\nDegrees travelled: ");
+  Serial.println(degreesTravelled());
+
+  bool machineIsHomingRising = machineIsHoming && !lastPedalUp ;
+
   lastPedalUp = pedalUp;
 
   // no homing information
   // default to stopping
-  if (pedalUp) {
-    if (zeroRising) {
-      stepper->forceStopAndNewPosition(0);
-      delay(500);
-    } else if (machineIsHomingRising) {
-      // Serial.println("Setting homing speed");
-      stepper->setSpeedInHz(homingSpeed);
-      stepper->move(5900);
-      // stepper->forceStopAndNewPosition(5700);
+  if (pedalUp && !(stepper->isStopping())) {
 
-      // Serial.println(stepper->getCurrentPosition());
+
+    Serial.println("Setting homing speed");
+    stepper->setSpeedInHz(homingSpeed);
+
+    int pulsesSoFar = myEnc.read();
+    int pulsesToGo = 2400 - pulsesSoFar;
+
+    int rawDegrees = pulsesSoFar / pulsesPerDegree;
+
+    int currentAngle = rawDegrees % 360;
+    int degreesToZero = 360 - currentAngle;
+
+    Serial.print("Current angle: ");
+    Serial.println(currentAngle);
+
+    int degreesToNeedleDown = degreesToZero + keyPositions.needleDown;
+    int degreesToNextValidStopPos = degreesToNeedleDown % 180;
+    
+    int stepsToTravel = degreesToNextValidStopPos * stepsPerDegree;
+    Serial.print("Steps to Travel: ");
+    Serial.println(stepsToTravel);
+
+    stepper->setCurrentPosition(0);
+
+    if (stepsToTravel > 100) {
+      stepper->moveTo(stepsToTravel, true); // blocking defeats zero rising sensor
+      while(stepper->getCurrentPosition() != stepsToTravel) {
+
+      }
+      Serial.println("confirm non blocking"); // blocking is actually blocking, not just a flag
+      stepper->forceStop();
+
+      delay(1000);
+
+      pulsesTravelled = 0;
+      delay(300);
     }
   } else {
+    Serial.println("else branch");
     stepper->setSpeedInHz(pedalSpeedInHz);
     stepper->runForward();
   }
-  delayMicroseconds(100);
+  // delayMicroseconds(100);
+}
+
+int degreesTravelled() {
+  if (pulsesTravelled == 0) {
+    return 0;
+  } else {
+    return pulsesTravelled / pulsesPerDegree;
+  }
 }
 
 void setStepsPerRotation() {
@@ -238,7 +223,29 @@ void setStepsPerRotation() {
  }
 }
 
-bool detectPedalUp(int pedalReading) {
+
+
+bool debounceHoming(bool pedalStatus) {
+
+  int numSamples = sizeof(homingSamples) / sizeof(homingSamples[0]);
+
+  bool allTrueFlag = true;
+
+  for (int i = numSamples - 1; i > 0; i--) {
+    homingSamples[i] = homingSamples[i-1];
+    if (allTrueFlag && !homingSamples[i]) {
+      allTrueFlag = false;
+    }
+  }
+
+  homingSamples[0] = pedalStatus;
+
+  return pedalStatus;
+}
+
+int updatePedalReading(int pedalReading) {
+  // TODO change to bit shift
+
   int numSamples = sizeof(pedalSamples) / sizeof(pedalSamples[0]);
 
   int samplesTotal = 0;
@@ -253,17 +260,19 @@ bool detectPedalUp(int pedalReading) {
 
   float avg = float(samplesTotal) / numSamples;
 
-  Serial.print("SampledAvg:");
-  Serial.print(avg);
-  Serial.print(",");
-  if (avg > pedalReadMax) {
-    pedalReadMax = avg;
-  }
+  // Serial.print("SampledAvg:");
+  // Serial.print(avg);
+  // Serial.print(",");
+  // if (avg > pedalReadMax) {
+  //   pedalReadMax = avg;
+  // }
 
-  Serial.print("Max:");
-  Serial.println(pedalReadMax);
+  // Serial.print("Max:");
+  // Serial.println(pedalReadMax);
 
-  return (avg < pedalPinMin);
+  // -2 offset is to catch false positives
+  // TODO globalize
+  return avg;
 }
 
 void needleDownInterrupt() {
